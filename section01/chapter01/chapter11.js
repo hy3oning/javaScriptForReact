25043ae26c1b4dfbaf034ec58dc2ccf14126cc3c
hy3oning <shyun5635@gmail.com>
2026-02-03 14:12:47 +0900

    주석

--- section01/chapter01/chapter11.js
+++ section01/chapter01/chapter11.js
@@ -46,13 +46,13 @@ console.log(getArea4)
 
 
 // 함수
-let area1 = getArea5(10, 20);
-console.log(area1);
-let area2 = getArea5(30, 20);
-console.log(area2);
-getArea5(120, 200);
-// 호이스팅이란 끌어올리다 라는 뜻
-function getArea5(width, height) {
- let area = width * height;
- return area;
-}
+// let area1 = getArea(10, 20);
+// console.log(area1);
+// let area2 = getArea(30, 20);
+// console.log(area2);
+// getArea(120, 200);
+// // 호이스팅이란 끌어올리다 라는 뜻
+// function getArea(width, height) {
+//  let area = width * height;
+//  return area;
+// }
