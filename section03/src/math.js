023dd9b2ce8784ba968c7d8562a5f33473522ffa
hy3oning <shyun5635@gmail.com>
2026-02-06 09:31:08 +0900

    [create] 리액트 생성

---
+++ section03/src/math.js
@@ -0,0 +1,11 @@
+//math 모듈 (객체 : function)
+export default function add(a, b) {
+  return a + b;
+}
+export function sub(a, b) {
+  return a - b;
+}
+
+export function div(a, b) {
+  return a / b;
+}
