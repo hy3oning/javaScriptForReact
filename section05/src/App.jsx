fec31968ef0f5531e4fadcfe88c31bd3b6e37bad
hy3oning <shyun5635@gmail.com>
2026-02-09 17:49:23 +0900

    [create] 랜더링

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,12 +1,10 @@
 import "./App.css";
-import Bulb from "./components/Bulb";
-import Count from "./components/Count";
-import Register from "./components/Register";
+import HookExam from "./components/HookExam";
 export default function App() {
   return (
     <>
       <div>
-        <Register />
+        <HookExam />
       </div>
     </>
   );
