79bfb6f30681d7562e1e17946b51801aa822f5db
hy3oning <shyun5635@gmail.com>
2026-02-09 11:21:07 +0900

    [update] useRef 테스트

--- section05/src/components/Register.jsx
+++ section05/src/components/Register.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useRef } from "react";
 //회원가입폼
 //1.이름 2.생년월일, 3.국적, 4.자기소개
 
@@ -20,18 +21,37 @@ const Register = () => {
   // const onChangeBio = (e) => {
   //   setInput({ ...input, bio: e.target.value });
   // };
-  //통합
+  //통합 (상태관리) useState
   const [input, setInput] = useState({
-    name: "이름",
+    name: "",
     birth: "",
     national: "",
-    bio: "자기소개",
+    bio: "",
   });
+
+  // useRef
+  const countRef = useRef(0);
+  const inputNameRef = useRef();
+  const inputBioRef = useRef();
+
+  //이벤트정의
   const onChange = (e) => {
+    // countRef.current++; 카운트
+    // console.log(countRef.current); 카운트
+    console.log(`current =${countRef.current}`);
     setInput({
       ...input,
       [e.target.name]: e.target.value,
     });
+  };
+  const onSubmit = (e) => {
+    if (input.name === "") {
+      inputNameRef.current.focus();
+      console.log(inputNameRef);
+    } else if (input.bio === "") {
+      inputBioRef.current.focus();
+      console.log(inputBioRef);
+    }
   };
   return (
     <>
@@ -39,6 +59,7 @@ const Register = () => {
         <label htmlFor="name">성명</label>
         <input
           value={input.name}
+          ref={inputNameRef}
           type="text"
           name="name"
           id="name"
@@ -73,12 +94,18 @@ const Register = () => {
         <label htmlFor="bio">자기소개</label>
         <textarea
           value={input.bio}
+          ref={inputBioRef}
           name="bio"
           id="bio"
           cols={30}
           rows={10}
           onChange={onChange}
         ></textarea>
+      </div>
+      <div>
+        <button type="button" onClick={onSubmit}>
+          제출
+        </button>
       </div>
     </>
   );
