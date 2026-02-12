81da77271f0857aedd5b0f3fee2cfb748bd50761
hy3oning <shyun5635@gmail.com>
2026-02-12 17:46:34 +0900

    [update] 버튼에 뒤로가기 추가

--- section13/src/components/Diary.jsx
+++ section13/src/components/Diary.jsx
@@ -1,21 +1,17 @@
 import Header from "./Header";
 import Button from "./Button";
 import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Diary = () => {
   const params = useParams();
+  const nav = useNavigate();
   return (
     <>
       <Header
         title={"HEADER"}
         leftChild={
-          <Button
-            text={"LEFT"}
-            type={"POSITIVE"}
-            onClick={(e) => {
-              alert(e.target.innerText);
-            }}
-          />
+          <Button text={"back"} type={"POSITIVE"} onClick={() => nav("/")} />
         }
         rightChild={
           <Button
