ddf106b21469144175d5a64afae4e0a7bf17fb3a
hy3oning <shyun5635@gmail.com>
2026-02-16 20:36:04 +0900

    [update]

--- section98/src/App.jsx
+++ section98/src/App.jsx
@@ -5,20 +5,38 @@ import NewBoard from "./components/NewBoard";
 import Login from "./components/Login";
 import NotFound from "./components/NotFound";
 import BoardDetail from "./components/BoardDetail";
+import { createContext, useReducer, useRef } from "react";
+import Reducer from "./context/Reducer";
+
+export const BoardStateContext = createContext();
+export const BoardDispatchContext = createContext();
 
 function App() {
+  const idRef = useRef(1);
+  const initialState = { author: "", title: "", content: "", posts: [] };
+  const [state, dispatch] = useReducer(Reducer, initialState);
+
+  // onCreate 정의
+  const onCreate = () => {
+    if (!state.author || !state.title || !state.content) {
+      alert("모든 항목을 입력해주세요!");
+      return;
+    }
+    dispatch({ type: "CREATE", id: idRef.current++ });
+  };
+
   return (
-    <>
-      <div className="app-wrapper">
+    <BoardStateContext.Provider value={state}>
+      <BoardDispatchContext.Provider value={{ dispatch, onCreate, idRef }}>
         <Routes>
-          <Route path="/" element={<Login />}></Route>
-          <Route path="/home" element={<Home />}></Route>
-          <Route path="/newBoard" element={<NewBoard />}></Route>
-          <Route path="/boardDetail/:id" element={<BoardDetail />}></Route>
-          <Route path="/*" element={<NotFound />}></Route>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/newBoard" element={<NewBoard />} />
+          <Route path="/boardDetail/:id" element={<BoardDetail />} />
+          <Route path="/*" element={<NotFound />} />
         </Routes>
-      </div>
-    </>
+      </BoardDispatchContext.Provider>
+    </BoardStateContext.Provider>
   );
 }
 
